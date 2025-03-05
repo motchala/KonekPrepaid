1392ed0f506a4daa64d86f55fd4bc877784a72bc
Frederick <Frederick@LAPTOP-F2GV11BB>
2025-03-06 04:48:26 +0800

    removed the unused variable.

--- KonekPrepaid/KonekApp.cs
+++ KonekPrepaid/KonekApp.cs
@@ -6,7 +6,7 @@ namespace KonekPrepaid
     {
         static void Main(string[] args)
         {
-            int loadBalance = 70, loadTotal = 70, loadAmnt, back = -1;
+            int loadBalance = 70, loadAmnt, back = -1;
             int choice1;
             int kon59 = 59,
                 kon99 = 99,
@@ -43,8 +43,8 @@ namespace KonekPrepaid
                             Console.WriteLine("\n\t! Returned you back to the Main Menu !");
                             continue;
                         }
-                        loadTotal += loadAmnt;
-                        Console.WriteLine("\n\t[Load Balance = " + loadTotal + "]");
+                        loadBalance += loadAmnt;
+                        Console.WriteLine("\n\t[Load Balance = " + loadBalance + "]");
 
                     }
                     else if (choice1 == 2)
@@ -65,11 +65,11 @@ namespace KonekPrepaid
                         }
                         else if (konChoice == 1)
                         {
-                            if (loadTotal >= kon59)
+                            if (loadBalance >= kon59)
                             {
                                 Console.WriteLine("\n\t\t[You're now subscribed to Konek59 promo.]");
                                 Console.Beep();
-                                loadTotal -= kon59;
+                                loadBalance -= kon59;
                                 Console.WriteLine("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
                                 continue;
                             }
@@ -80,11 +80,11 @@ namespace KonekPrepaid
                         }
                         else if (konChoice == 2)
                         {
-                            if (loadTotal >= kon99)
+                            if (loadBalance >= kon99)
                             {
                                 Console.WriteLine("\n\t\t[You're now subscribed to Konek99 promo.]");
                                 Console.Beep();
-                                loadTotal -= kon99;
+                                loadBalance -= kon99;
                                 Console.WriteLine("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
                                 continue;
                             }
@@ -95,11 +95,11 @@ namespace KonekPrepaid
                         }
                         else if (konChoice == 3)
                         {
-                            if (loadTotal >= kon149)
+                            if (loadBalance >= kon149)
                             {
                                 Console.WriteLine("\n\t\t[You're now subscribed to Konek149 promo.]");
                                 Console.Beep();
-                                loadTotal -= kon149;
+                                loadBalance -= kon149;
                                 Console.WriteLine("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
                                 continue;
                             }
@@ -110,11 +110,11 @@ namespace KonekPrepaid
                         }
                         else if (konChoice == 4)
                         {
-                            if (loadTotal >= kon300)
+                            if (loadBalance >= kon300)
                             {
                                 Console.WriteLine("\n\t\t[You're now subscribed to Konek300 promo.]");
                                 Console.Beep();
-                                loadTotal -= kon300;
+                                loadBalance -= kon300;
                                 Console.WriteLine("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
 
                                 continue;
@@ -131,7 +131,7 @@ namespace KonekPrepaid
                     }
                     else if (choice1 == 3)
                     {
-                        Console.WriteLine("\n\t[Load Balance: " + loadTotal + "]");
+                        Console.WriteLine("\n\t[Load Balance: " + loadBalance + "]");
                         continue;
                     }
                     else
