d2ad2f28a3558f2e067362f621f06131b7241ee2
Frederick Rosales <iamfrederickr@gmail.com>
2025-03-28 02:30:49 +0800

    Main Class
    this is the main thing. eto na talaga sya haha

---
+++ KonekApplication/Program.cs
@@ -0,0 +1,194 @@
+﻿using KonekLogicProcess;
+namespace KonekApplication
+{
+    public class Program
+    {
+        BusinessDataLogic BDL = new BusinessDataLogic();
+        public Program()
+        {
+            mainMenu();
+        }
+
+
+
+
+        public void mainMenu()
+        {
+            Console.WriteLine("\t\t\tWelcome to Konek App!");
+            BorderText();
+
+            while (true)
+            {
+                Console.WriteLine("MAIN MENU:\n\n" +
+                    "[1] Buy Load\n" +
+                    "[2] Buy Promos\n" +
+                    "[3] Check Balance\n" +
+                    "[0] Exit App\n");
+                Console.Write("Choose menu: ");
+                BusinessDataLogic.choiceMenu = Convert.ToInt32(Console.ReadLine());
+
+                if (BusinessDataLogic.choiceMenu == 0)
+                {
+                    BorderText();
+                    Environment.Exit(0);                        // code to exit/terminate program
+                }
+                else if (BusinessDataLogic.choiceMenu == 1)
+                {
+                    LoadMenu();
+                }
+                else if (BusinessDataLogic.choiceMenu == 2)
+                {
+                    PromoMenu();                                // promo menu
+                }
+                else if (BusinessDataLogic.choiceMenu == 3)
+                {
+                    BalanceDisplay();
+                }
+                else
+                {
+                    ErrorDisplay();
+                }
+            }
+        }
+
+
+
+
+        public void LoadMenu()
+        {
+            BorderText();
+            Console.WriteLine("\tI. LOAD MENU:\n");
+            Console.Write("\tEnter Load Amount: ");
+            BusinessDataLogic.loadAmount = Convert.ToInt32(Console.ReadLine());
+
+            if (BusinessDataLogic.loadAmount == BusinessDataLogic.secretBack)
+            {
+                HalfBorderText();
+                Console.WriteLine("! Returning to main menu... !");
+                HalfBorderText(); Thread.Sleep(2200); // a visual delay
+            }
+            else
+            {
+                BusinessDataLogic.LoadProcess();
+                LoadSuccessDisplay();
+                Thread.Sleep(500);
+                BorderText();
+            }
+            
+        }
+
+
+        public void PromoMenu()
+        {
+            BorderText();
+            Console.WriteLine("\tII. PROMO MENU:\n\n" +
+                "\t[1] Konek59\n" +
+                "\t[2] Konek99\n" +
+                "\t[3] Konek149\n" +
+                "\t[4] Konek300\n" +
+                "\t[0] Back\n");
+            Console.Write("\tChoose promo: ");
+            BusinessDataLogic.choicePromo = Convert.ToInt32(Console.ReadLine());
+
+            if (BusinessDataLogic.choicePromo == 0) // returns you to main menu
+            {
+                BorderText();
+                return;
+            }
+            else if (BusinessDataLogic.choicePromo < 1 || BusinessDataLogic.choicePromo > 4)
+            {
+                ErrorDisplay();
+                return;
+            }
+
+            if (BusinessDataLogic.CanPurchasePromo())
+            {
+                BusinessDataLogic.LoadBalUpdate();
+                PromoNamer();
+                PromoSuccessDisplay();
+                BorderText();
+                SoundEffects();
+            }
+            else if (!BusinessDataLogic.CanPurchasePromo())
+            {
+                Insufficient();
+            }
+        }
+
+
+        public void PromoNamer()
+        {
+            switch (BusinessDataLogic.choicePromo)
+            {
+                case 1: BusinessDataLogic.promoName = "Konek59"; break;
+                case 2: BusinessDataLogic.promoName = "Konek99"; break;
+                case 3: BusinessDataLogic.promoName = "Konek149"; break;
+                case 4: BusinessDataLogic.promoName = "Konek300"; break;
+                default: BusinessDataLogic.promoName = "! unknown !"; break;
+            }
+        }
+
+
+
+        public void BalanceDisplay()
+        {
+            BorderText();
+            Console.WriteLine("\t[Current Load Balance = " + BusinessDataLogic.loadBalance + "]");
+            BorderText();
+        }
+
+
+
+
+
+        // Designs lang palamuti
+        public void BorderText()
+        {
+            Console.WriteLine("\n---------------------------------------------------------------------------\n");
+        }
+        public void HalfBorderText()
+        {
+            Console.WriteLine("\n- - - - - - - - - - - - - - - - - - -\n");
+        }
+        public void SoundEffects() // for when successful promo subscription
+        {
+            Console.Beep();
+        }
+       
+        public void LoadSuccessDisplay()
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine("\n\t[Successful Load Transaction]");
+        }
+        public void Insufficient()
+        {
+            HalfBorderText();
+            Console.WriteLine("\t! Load Insufficient ! ");
+            HalfBorderText();
+        }
+        public void PromoSuccessDisplay()
+        {
+            Console.WriteLine("\n\t\t[You're now subscribed to " + BusinessDataLogic.promoName + " promo]");
+        }
+        public void ErrorDisplay()
+        {
+            HalfBorderText();
+            Console.WriteLine(" ! Invalid Choice ! "); 
+            HalfBorderText();
+        }
+
+
+
+
+
+
+
+
+
+        public static void Main(string[] args)
+        {
+            new Program();
+        }
+
+    }
+}
---
+++ KonekLogicProcess/BusinessDataLogic.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace KonekLogicProcess
+{
+    public class BusinessDataLogic
+    {
+        public static int choiceMenu;           // getting main menu input
+        public static int loadAmount;           // getting load amount input
+        public static int secretBack = -1;      // secret number to return to main menu
+        public static int choicePromo;          // getting promo menu choice 0-4
+        public static int loadBalance = 70;     // initial but dynamic load balance value
+        public static int kon59 = 59,           // coresponding value of available promos
+                          kon99 = 99,
+                          kon149 = 149,
+                          kon300 = 300;
+        public static string promoName;
+
+
+        // adds the purchased load to the load balance
+        public static void LoadProcess()
+        {
+            BusinessDataLogic.loadBalance += BusinessDataLogic.loadAmount;
+        }
+
+        // checks if load balance is sufficient for purchasing a promo
+        public static bool CanPurchasePromo()
+        {
+            int[] promoPrices = { 0, kon59, kon99, kon149, kon300 };
+            if (choicePromo >= 1 && choicePromo <= 4)
+            {
+                return loadBalance >= promoPrices[choicePromo];
+            }
+                return false;
+        }
+
+        // updates the load balance after a promo purchase
+        public static void LoadBalUpdate()
+        {
+            int[] promoPrices = { 0, kon59, kon99, kon149, kon300 };
+
+            if (choicePromo >= 1 && choicePromo <= 4)
+            {
+                loadBalance -= promoPrices[choicePromo];
+            }
+        }
+    }
+}
+
+
+
